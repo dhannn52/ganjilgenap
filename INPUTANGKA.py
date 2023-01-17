101820b1376a759da392022ff2ab3c151e52d63a
dhannn52 <122761757+dhannn52@users.noreply.github.com>
2023-01-17 14:14:57 +0700

    Add files via upload

---
+++ INPUTANGKA.py
@@ -0,0 +1,7 @@
+angka = int(input("Masukan Angka: "))
+
+if (angka % 2) == 0:
+    print("{0} adalah bilangan genap".format(angka))
+
+else:
+    print("{0} adalah bilangan ganjil".format(angka))
---
+++ nilai tugas.py
@@ -0,0 +1,10 @@
+tugas =int(input("Masukan nilai tugas:"))
+
+if tugas >= 80:
+    print("A")
+elif tugas >= 70:
+    print ("B")
+elif tugas >= 60:
+    print ("c")
+elif tugas >= 50:
+    print ( "D")
